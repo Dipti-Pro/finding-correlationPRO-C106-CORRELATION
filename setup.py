6c2489689b2572277dcf06e31572b118302ce57b
Dipti-Pro <85604091+Dipti-Pro@users.noreply.github.com>
2021-09-16 22:43:36 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,11 +23,4 @@ def findCorrelation(datasource):
     correlation = np.corrcoef(datasource["x"], datasource["y"])
     print("Correlation between Marks in percentage and Days present :-  \n--->",correlation[0,1])
 
-def setup():
-    data_path  = "./data/Student Marks vs Days Present.csv"
 
-    datasource = getDataSource(data_path)
-    findCorrelation(datasource)
-    plotFigure(data_path)
-
-setup()
